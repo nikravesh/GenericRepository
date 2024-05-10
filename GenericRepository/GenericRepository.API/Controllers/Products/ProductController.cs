d55a9be16faee4c97e221ecd1c5b7f269f1d3ea1
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-05-10 20:47:00 +0330

    added product controller

---
+++ GenericRepository/GenericRepository.API/Controllers/Products/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using GenericRepository.Domain.Products.Repository;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericRepository.API.Controllers.Products;
+public class ProductController : Controller
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    [HttpPost("{id}")]
+    public async Task<IActionResult> GetProduct(long productId)
+       => Ok(await _productRepository.GetByIdAsync(productId));
+}
--- GenericRepository/GenericRepository.Domain/Products/Repository/IProductRepository.cs
+++ GenericRepository/GenericRepository.Domain/Products/Repository/IProductRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace GenericRepository.Domain.Products.Repository;
-public interface IProductRepository
+﻿using GenericRepository.Domain.Base;
+using GenericRepository.Domain.Products.Entities;
+
+namespace GenericRepository.Domain.Products.Repository;
+public interface IProductRepository : IBaseRepository<Product,long>
 {
 }
